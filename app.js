1e9649e095999bcae6d202d2a08c8135aec3237b
Musmaxim <77632944+Musmaxim@users.noreply.github.com>
2022-07-22 16:31:46 +0300

    доработка для работы с фронтом 1.5

--- app.js
+++ app.js
@@ -25,7 +25,14 @@ app.use(requestLogger);
 
 app.use(limiter);
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://frontend.musmaxim.nomoreparties.sbs',
+    'https://frontend.musmaxim.nomoreparties.sbs',
+  ],
+  methods: ['OPTIONS', 'GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  credentials: true,
+}));
 
 app.use(router);
 
